afb35bae4e6c8768a1c5a897c7cd6b86bc40b8b1
denis <denis.denisenko@gmai.com>
2023-05-05 21:44:29 +0300

    stam

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          This is DD
+          This is DD and Nadia
         </p>
         <a
           className="App-link"
