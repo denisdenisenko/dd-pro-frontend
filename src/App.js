3f0b5ea2eccf351904e1e678d2b5b0228ad61e84
denis <denis.denisenko@gmai.com>
2023-05-06 23:07:27 +0300

    added new svg image

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import cat from './cat.svg';
 import './App.css';
 import Button from './Components/Button';
 import Cookie from './Components/Cookie';
@@ -9,6 +10,7 @@ function App() {
       <header className="App-header">
         <Cookie/>
         <img src={logo} className="App-logo" alt="logo" />
+        <img className = "catImg" src ={cat} alt="cat"/>
         <Button color= "color"  text ="Get Users"/>       
       </header>
     </div>
